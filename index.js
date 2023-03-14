187aaec851c4a80b7dd575bd2e7ffde2fc7b219e
gokul85 <gokulkumar5505@gmail.com>
2023-03-14 16:04:04 +0530

    test commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req, res)=>{
     res.status(200).send("Backend");
 })
 
+app.post('/',(req, res)=>{
+    res.status(200).send("Test Data");
+})
+
 app.listen("5000",()=>{
     console.log("Backend is Running");
 })
