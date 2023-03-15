cd9af0124246bb38a6641d839cef1b48e60ee403
gokul85 <gokulkumar5505@gmail.com>
2023-03-15 15:46:29 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require("./routes/auth");
 
-app.get('/',(req, res)=>{
-    res.status(200).send("Backend");
-})
+dotenv.config();
+app.use(express.json());
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-app.post('/',(req, res)=>{
-    res.status(200).send("Test Data");
+app.use("/auth", authRoute);
+app.get('/',(req,res)=>{
+    res.status(200).send("Backend is Running and Mongodb Connected")
 })
 
 app.listen("5000",()=>{
---
+++ routes/auth.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require("bcrypt");
+
+router.post('/', async (req,res) => {
+    try {
+        const username = await User.findOne({ username: req.body.username });
+        const email = await User.findOne({ email: req.body.email });
+        if (username) {
+        res.status(200).json({ err: "Username Not Available" })
+        } else if (email) {
+        res.status(200).json({ err: "Email already Registered" })
+        }
+        else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
+            const newUser = new User({
+              username: req.body.username,
+              email: req.body.email,
+              password: hashedPass,
+            });
+      
+            const newuser = await newUser.save();
+            const user = await User.findOne({ username: req.body.username });
+            const { password, ...others } = user._doc;
+            res.status(200).json(others);
+          }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        res.status(200).json({ err: "Invalid Username!" })
+      } else {
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      if (!validated) {
+        res.status(200).json({ err: "Wrong Password!" })
+      } else {
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+      }
+    }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
